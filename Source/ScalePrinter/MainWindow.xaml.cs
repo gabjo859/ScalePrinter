90217fbdb5f5fbf205915303b60fbcc8a68f844e
Gabriel Söderlund <gabjo859@gmail.com>
2015-05-16 08:52:24 +0200

    Start working on GUI

--- Source/ScalePrinter/MainWindow.xaml.cs
+++ Source/ScalePrinter/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScalePrinter.Client.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,7 @@ namespace ScalePrinter.Client {
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
+            this.DataContext = new MainViewModel();
             InitializeComponent();
         }
     }
