d361547b2bd5f2f85aed052248c2807ebce30fa9
Gabriel Söderlund <gabjo859@gmail.com>
2015-06-10 22:47:55 +0200

    Simple user interface for selecting HID device for testing

--- Source/ScaleTest/Program.cs
+++ Source/ScaleTest/Program.cs
@@ -7,25 +7,53 @@ using System.Threading.Tasks;
 
 namespace ScaleTest {
     class Program {
+
+        private static IEnumerable<HidDevice> allDevices = HidDevices.Enumerate();
+
         static void Main(string[] args) {
 
-            var allDevices = HidDevices.Enumerate();
-
-            if (allDevices.Any()) {
-                foreach (var device in allDevices) {
-                    Console.WriteLine("\n\nDescription: " + device.Description);
-                    Console.WriteLine(String.Concat(Enumerable.Range(0, device.Description.Length).Select(x => "=").ToArray()));
-                    Console.WriteLine("ProductId:" + device.Attributes.ProductId);
-                    Console.WriteLine("ProductHexId:" + device.Attributes.ProductHexId);
-                    Console.WriteLine("VendorId:" + device.Attributes.VendorId);
-                    Console.WriteLine("VendorHexId:" + device.Attributes.VendorHexId);
-                    Console.WriteLine("Version:" + device.Attributes.Version);
-                    Console.WriteLine("Path: " + device.DevicePath);
-                }
-            } else {
-                Console.WriteLine("Found no HID devices..:");
+            if (!allDevices.Any()) {
+                Console.WriteLine("Found no HID devices...");
+                Exit();
             }
 
+            DisplayDeviceInfo(allDevices);
+
+            HidDevice selectedDevice = null;
+            while (true) {
+                selectedDevice = GetSelectedDevice();
+                Console.WriteLine(String.Format("\nYou selected '{0}'", selectedDevice.Description));
+            }
+        }
+
+        private static HidDevice GetSelectedDevice() {
+            Console.Write("\nSelect a device for further testing, or 'q' to exit: ");
+            var input = Console.ReadLine();
+            if (input.ToLower() == "q") Environment.Exit(0);
+            try {
+                int index = int.Parse(input) - 1;
+                return allDevices.ElementAt(index);
+            } catch (Exception e) {
+                Console.WriteLine("\nERROR: Invalid choice: " + input + ". Must a any of the numbers in the list of HID devices. Try again...");
+                return GetSelectedDevice();
+            }
+        }
+
+        private static void DisplayDeviceInfo(IEnumerable<HidDevice> devices) {
+            int i = 0;
+            foreach (var device in devices) {
+                i++;
+                Console.WriteLine(String.Format("\n\n{0}. {1}", i, device.Description));
+                Console.WriteLine(String.Concat(Enumerable.Range(0, device.Description.Length).Select(x => "=").ToArray()));
+                Console.WriteLine("ProductId:" + device.Attributes.ProductId);
+                Console.WriteLine("ProductHexId:" + device.Attributes.ProductHexId);
+                Console.WriteLine("VendorId:" + device.Attributes.VendorId);
+                Console.WriteLine("VendorHexId:" + device.Attributes.VendorHexId);
+                Console.WriteLine("Version:" + device.Attributes.Version);
+            }
+        }
+
+        private static void Exit() {
             Console.WriteLine("\n\nPress any key to exit...");
             Console.ReadKey();
             Environment.Exit(0);
